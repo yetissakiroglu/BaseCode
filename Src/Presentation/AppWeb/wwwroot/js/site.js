9b75caf68f66d5047546c55e97285623fe82d1c8
yetis <yetis@MSI>
2025-02-17 23:08:26 +0300

    feat :

--- Src/Presentation/AppWeb/wwwroot/js/site.js
+++ Src/Presentation/AppWeb/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.carousel_in').owlCarousel({
+    center: true,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    navText: ['', ''],
+    addClassActive: true,
+    margin: 5,
+    responsive: {
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2,
+            nav: true,
+        }
+    }
+});
