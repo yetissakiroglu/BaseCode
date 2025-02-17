38cc33b75f76184bc90e5cc376dc108ff08a4928
yetis <yetis@MSI>
2025-02-17 23:18:45 +0300

    feat :

--- Src/Presentation/AppWeb/Program.cs
+++ Src/Presentation/AppWeb/Program.cs
@@ -1,8 +1,16 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), configure =>
+    {
+        configure.MigrationsAssembly("Economy.Persistence");
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
